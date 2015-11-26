6e36ebdf767a3d29bd1c6d4e1e6204616d49e2c4
crystal511 <crystalsome@gmail.com>
2015-11-26 11:54:38 -0800

    Create ValidAnagram.java

---
+++ ValidAnagram.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+        int[] table = new int[26];
+        for (char c : s.toCharArray()){
+            table[c-'a']++;
+        }
+        for (char c : t.toCharArray()){
+            table[c-'a']--;
+            if (table[c-'a'] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
